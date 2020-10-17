d0f83db070e6f733b83b91c20504d2bb95719cfd
郑新越 <zhengxinyue@polyv.net>
2020-10-17 17:40:47 +0800

    spark基础练习题

--- src/main/scala/practise/Practise1.scala
+++ src/main/scala/practise/Practise1.scala
@@ -12,9 +12,9 @@ object Practise1 {
     val conf = new SparkConf().setAppName("practise").setMaster("local")
     val sc = new SparkContext(conf)
 
-    val lines1 = sc.textFile("C:\\workspace-mine\\SparkDemo\\src\\main\\resources\\practise\\practise1\\file1.txt")
-    val lines2 = sc.textFile("C:\\workspace-mine\\SparkDemo\\src\\main\\resources\\practise\\practise1\\file2.txt")
-    val lines3 = sc.textFile("C:\\workspace-mine\\SparkDemo\\src\\main\\resources\\practise\\practise1\\file3.txt")
+    val lines1 = sc.textFile("C:\\workspace-mine\\SparkDemo\\src\\main\\resources\\practise\\practise1\\file1-1.txt")
+    val lines2 = sc.textFile("C:\\workspace-mine\\SparkDemo\\src\\main\\resources\\practise\\practise1\\file2-1.txt")
+    val lines3 = sc.textFile("C:\\workspace-mine\\SparkDemo\\src\\main\\resources\\practise\\practise1\\file3-1.txt")
     val linesAll = lines1.union(lines2).union(lines3)
 
     //假如是想按首字母排序，不需要做Integer::valueOf的类型转换
