aca03dc886436bcf6a6a3379b3c3e60801fa404a
roderickzxy <roderickzxy@qq.com>
2020-10-18 16:48:10 +0800

    spark基础练习题2

---
+++ src/main/java/com/rode/spark/practise/Practise2.java
@@ -0,0 +1,11 @@
+package com.rode.spark.practise;
+
+/**
+ * 题目2：
+ * 给定键值对，("spark",2),("hadoop",6),("hadoop",4),("spark",6)，键值对的key，
+ * 表示图书名称，value表示某天图书销量，请计算每个键对应的平均值，也就是计算每种图书的每天平均销量
+ * @author zhengxinyue
+ * @since 2020/10/18
+ */
+public class Practise2 {
+}
