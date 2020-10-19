edf349405f48c9484da2281ba049d2a4b3b9fd8a
roderickzxy <roderickzxy@qq.com>
2020-10-19 14:06:52 +0800

    spark基础练习题

---
+++ src/main/scala/practise/Practise2.scala
@@ -0,0 +1,21 @@
+package practise
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+
+/**
+  *
+  * @author zhengxinyue
+  * @since 2020/10/19
+  */
+object Practise2 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("practise1").setMaster("local")
+    val sc = new JavaSparkContext(conf)
+    val lines = sc.parallelize(List(("spark", 2), ("hadoop", 6), ("hadoop", 2), ("spark", 6), ("hive", 200), ("hbase", 1)))
+    val counts = lines.reduceByKey((v1, v2) => (v1 + v2) / 2)
+
+    val sortPairRdd = counts.map(v=>(v._2,v._1)).sortByKey()
+    sortPairRdd.map(v=>(v._2,v._1)).collect.foreach(println)
+  }
+}
