b4e26ca394ab7201dd863db89c20250f5cb84524
roderickzxy <roderickzxy@qq.com>
2020-10-21 00:11:41 +0800

    练习题3

--- src/main/java/com/rode/spark/practise/PractiseConstants.java
+++ src/main/java/com/rode/spark/practise/PractiseConstants.java
@@ -2,7 +2,7 @@ package com.rode.spark.practise;
 
 public class PractiseConstants {
 
-//    public static final String CLASS_PATH = "/home/roderick/IdeaProjects/SparkDemo/src/main/resources/";
+    public static final String CLASS_PATH = "/home/roderick/IdeaProjects/SparkDemo/src/main/resources/";
 
-    public static final String CLASS_PATH = "C:/workspace-mine/SparkDemo/src/main/resources/";
+//    public static final String CLASS_PATH = "C:/workspace-mine/SparkDemo/src/main/resources/";
 }
