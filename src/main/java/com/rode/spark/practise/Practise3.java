1a4b1233fb025ca76e8e434085afd959147a5016
roderickzxy <roderickzxy@qq.com>
2020-10-21 13:30:47 +0800

    spark基础练习题

--- src/main/java/com/rode/spark/practise/Practise3.java
+++ src/main/java/com/rode/spark/practise/Practise3.java
@@ -35,8 +35,8 @@ import scala.math.Ordered;
  */
 public class Practise3 {
     public static void main(String[] args) {
-//        practise1();
-        practise2();
+        practise1();
+//        practise2();
     }
     
     /**
@@ -77,69 +77,69 @@ public class Practise3 {
         }).collect().forEach(System.out::println);
         
     }
-
-
-}
-
-/**
- * 自定义排序的类型
- */
-class UDFSort implements Serializable, Ordered<UDFSort> {
-    private String k1;
-    private Integer k2;
-
-    public UDFSort(String k1, Integer k2) {
-        this.k1 = k1;
-        this.k2 = k2;
-    }
-
-    public String getK1() {
-        return k1;
-    }
-
-    public void setK1(String k1) {
-        this.k1 = k1;
-    }
-
-    public Integer getK2() {
-        return k2;
-    }
-
-    public void setK2(Integer k2) {
-        this.k2 = k2;
-    }
-
-    @Override
-    public int compare(UDFSort that) {
-        if (!k1.equals(that.getK1())) {
-            return this.k1.compareTo(that.getK1());
+    
+    
+    /**
+     * 自定义排序的类型
+     */
+    static class UDFSort implements Serializable, Ordered<UDFSort> {
+        private String k1;
+        private Integer k2;
+        
+        public UDFSort(String k1, Integer k2) {
+            this.k1 = k1;
+            this.k2 = k2;
         }
-
-        return this.k2.compareTo(that.getK2());
+        
+        public String getK1() {
+            return k1;
+        }
+        
+        public void setK1(String k1) {
+            this.k1 = k1;
+        }
+        
+        public Integer getK2() {
+            return k2;
+        }
+        
+        public void setK2(Integer k2) {
+            this.k2 = k2;
+        }
+        
+        @Override
+        public int compare(UDFSort that) {
+            if (!k1.equals(that.getK1())) {
+                return this.k1.compareTo(that.getK1());
+            }
+            
+            return this.k2.compareTo(that.getK2());
+        }
+        
+        @Override
+        public boolean $less(UDFSort that) {
+            return compare(that) < 0;
+        }
+        
+        @Override
+        public boolean $greater(UDFSort that) {
+            return compare(that) > 0;
+        }
+        
+        @Override
+        public boolean $less$eq(UDFSort that) {
+            return compare(that) <= 0;
+        }
+        
+        @Override
+        public boolean $greater$eq(UDFSort that) {
+            return compare(that) >= 0;
+        }
+        
+        @Override
+        public int compareTo(UDFSort that) {
+            return compare(that);
+        }
     }
 
-    @Override
-    public boolean $less(UDFSort that) {
-        return compare(that) < 0;
-    }
-
-    @Override
-    public boolean $greater(UDFSort that) {
-        return compare(that) > 0;
-    }
-
-    @Override
-    public boolean $less$eq(UDFSort that) {
-        return compare(that) <= 0;
-    }
-
-    @Override
-    public boolean $greater$eq(UDFSort that) {
-        return compare(that) >= 0;
-    }
-
-    @Override
-    public int compareTo(UDFSort that) {
-        return compare(that);
-    }
 }
