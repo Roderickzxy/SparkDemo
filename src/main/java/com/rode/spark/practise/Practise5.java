6230568c6a4cf649c92f1d06ccf74fd413f02db6
roderickzxy <roderickzxy@qq.com>
2020-11-04 14:20:07 +0800

    练习题5

---
+++ src/main/java/com/rode/spark/practise/Practise5.java
@@ -0,0 +1,26 @@
+package com.rode.spark.practise;
+
+/**
+ * 5.单表关联
+ *     题目：求孙子和祖父母的关系列表
+ *     数据：   child        parent
+ *                 Tom        Lucy
+ *                 Tom        Jack
+ *                 Jone        Lucy
+ *                 Jone        Jack
+ *                 Lucy        Mary
+ *                 Lucy        Ben
+ *                 Jack        Alice
+ *                 Jack        Jesse
+ *                 Terry        Alice
+ *                 Terry        Jesse
+ *                 Philip        Terry
+ *                 Philip        Alma
+ *                 Mark        Terry
+ *                 Mark        Alma
+ */
+public class Practise5 {
+    public static void main(String[] args) {
+
+    }
+}
